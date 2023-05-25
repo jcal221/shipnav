0e2964aca1199ecfb87a408a39ded8794caf9d6d
jcal221 <115591229+jcal221@users.noreply.github.com>
2023-05-25 15:10:17 +0200

    Full-Stack Immersion Exercise 1.5
    If applied, this commit will have applied all subtasks requested in Full-Stack Immersion
    Exercise 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,43 @@
-let pokemonList = [
-    {
-      name: "Bulbasaur",
-      height: 0.7,
-      types: ["grass", "poison"]
-    },
-    {
-      name: "Charizard",
-      height: 1.7,
-      types: ["fire", "flying"]
-    },
-    {
-      name: "Squirtle",
-      height: 1.0,
-      types: ["water"]
-    }
-  ];
+let pokemonRepository = (function() {
+    let pokemonList = [
+      {
+        name: "Bulbasaur",
+        height: 0.7,
+        types: ["grass", "poison"]
+      },
+      {
+        name: "Charizard",
+        height: 1.7,
+        types: ["fire", "flying"]
+      },
+      {
+        name: "Squirtle",
+        height: 1.0,
+        types: ["water"]
+      }
+    ];
   
-  for (let i = 0; i < pokemonList.length; i++) {
-    let pokemon = pokemonList[i];
+    return {
+      getAll: function() {
+        return pokemonList;
+      },
+      add: function(pokemon) {
+        pokemonList.push(pokemon);
+      }
+    };
+  })();
+  
+  pokemonRepository.getAll().forEach(function(pokemon) {
     let output = pokemon.name + " (height: " + pokemon.height + ")";
-    
+  
     if (pokemon.height > 1.0) {
       output += " - Wow, that's big!";
     }
-    
+  
     document.write(output + "<br>");
-  }
+  });
+  
+  console.log(pokemonRepository.getAll()); // Logs the pokemonList array
+  pokemonRepository.add({ name: "Pikachu", height: 0.4, types: ["electric"] });
+  console.log(pokemonRepository.getAll()); // Logs the updated pokemonList array
+  
